505a2154c2b6143eba8596426984dd425a3cb817
OmChauhan07 <odchauhan0702@gmail.com>
2026-02-01 23:17:38 +0530

    Create preload.js

---
+++ DAO_Browser/src/preload/preload.js
@@ -0,0 +1,14 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// We "expose" specific functions to the renderer (UI)
+contextBridge.exposeInMainWorld('electronAPI', {
+    // Health check for Python backend
+    checkHealth: () => ipcRenderer.invoke('api:health'),
+    
+    // Ad-Blocker API
+    adBlocker: {
+        getStats: () => ipcRenderer.invoke('adBlocker:getStats'),
+        toggle: () => ipcRenderer.invoke('adBlocker:toggle'),
+        resetSession: () => ipcRenderer.invoke('adBlocker:resetSession')
+    }
+});
